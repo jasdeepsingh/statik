eed7779f6611c37513db2c08b25ddc06e4e6c681
Jatinder Singh <j.singh.developer@gmail.com>
2012-06-12 17:27:39 -0400

    Initial changes covers nozing running of tests

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Statik::Application.config.secret_token = 'dd56756a55e668b837d0ee89c19d69084fb062e27c729d7c68ef4eb6800f252f68372b542a4836275f453cb7d449c69c7e031c7cc39b4b3a4cf873344037fad1'
